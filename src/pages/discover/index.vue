9d9ac4e84d068194bb8093134a278b074882a9f8
heyangjie <1059320200@qq.com>
2022-10-08 11:14:40 +0800

    列表组件

--- src/pages/discover/index.vue
+++ src/pages/discover/index.vue
@@ -2,10 +2,18 @@
   import Taro from '@tarojs/taro';
   import NavBar from '@/components/NavBar';
   import Info from '@/components/ListItem/Info';
+  import List from '@/components/List';
   import { useSystemInfoStore } from '@/store';
   import styles from './index.module.scss';
 
   const systemInfo = useSystemInfoStore();
+  const fetchApi = async () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    });
+  };
   const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 </script>
 
@@ -15,5 +23,16 @@
     <view :class="styles.box">
       <Info v-for="(item, index) in list" :key="index" />
     </view>
+    <List :fetchApi="fetchApi">
+      <template #header>
+        <div>header</div>
+      </template>
+      <template #item="{data}">
+        <div>{{ data }}</div>
+      </template>
+      <template #footer>
+        <div>footer</div>
+      </template>
+    </List>
   </view>
 </template>
